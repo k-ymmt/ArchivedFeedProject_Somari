bf02c5a4d5a254ca8a73d6a81c82bcdf6a4e8184
k-ymmt <account@kymmt.net>
2019-10-05 20:07:26 +0900

    Update: Colors

--- App/Somari/Somari/Resources/Colors.swift
+++ App/Somari/Somari/Resources/Colors.swift
@@ -12,18 +12,20 @@ import UIKit
 struct Colors {
     struct Color {
         private let name: String
-        lazy var uiColor: UIColor = {
-            return UIColor(named: name)!
-        }()
+        let uiColor: UIColor
         
         fileprivate init(from string: String) {
             self.name = string
+            self.uiColor = UIColor(named: string)!
         }
     }
 
     static let background = Color(from: "background")
-    static let foreground_second = Color(from: "foreground_second")
+    static let background_second = Color(from: "background_second")
     static let foreground = Color(from: "foreground")
+    static let foreground_second = Color(from: "foreground_second")
     static let orange = Color(from: "orange")
     static let red = Color(from: "red")
+    static let blue = Color(from: "blue")
+    static let aqua = Color(from: "aqua")
 }
