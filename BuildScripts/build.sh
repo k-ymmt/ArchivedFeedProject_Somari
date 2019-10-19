75ab2935b2a3bf7002e4f0bbfdbca80ead7d8d8b
k-ymmt <account@kymmt.net>
2019-10-19 20:14:17 +0900

    Update: improve build scripts

--- BuildScripts/build.sh
+++ BuildScripts/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -ex
+
 dir=$(cd $(dirname $0);pwd)
+export_dir="${dir}/build"
+app_name="Somari"
 
 if ! type bundle 2>&1 >/dev/null;then
   echo 'required bundle'
@@ -16,6 +20,13 @@ bundle exec pod install
 popd
 
 pushd $dir
-$dir/build_pod.sh
+$dir/build_pod.sh "ios"
 
 bundle exec fastlane build
+
+mkdir -p "$export_dir"
+
+find $dir -name "${app_name}.ipa" \
+  -o -name "${app_name}.app" \
+  -o -name "${app_name}.app.dSYM.zip" \
+  | xargs -I{} mv {} "${export_dir}/"
--- BuildScripts/build_pod.sh
+++ BuildScripts/build_pod.sh
@@ -4,6 +4,8 @@
 
 set -exo pipefail
 
+config=${1:-"ALL"}
+
 PROJECT_ROOT=$(cd $(dirname $0);cd ../App; pwd)
 PODS_ROOT="$PROJECT_ROOT/Pods"
 PODS_PROJECT="$PODS_ROOT/Pods.xcodeproj"
@@ -13,13 +15,17 @@ cd "$PROJECT_ROOT"
 bundle exec pod repo update
 COCOAPODS_DISABLE_STATS=true bundle exec pod install
 
-xcodebuild -project "$PODS_PROJECT" \
-  -sdk iphoneos -configuration Release -alltargets \
-  ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=NO SYMROOT="$SYMROOT" \
-  CLANG_ENABLE_MODULE_DEBUGGING=NO \
-  BITCODE_GENERATION_MODE=bitcode
+if [ $config != "simulator" ];then
+  xcodebuild -project "$PODS_PROJECT" \
+    -sdk iphoneos -configuration Release -alltargets \
+    ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=NO SYMROOT="$SYMROOT" \
+    CLANG_ENABLE_MODULE_DEBUGGING=NO \
+    BITCODE_GENERATION_MODE=bitcode
+fi
 
-xcodebuild -project "$PODS_PROJECT" \
-  -sdk iphonesimulator -configuration Release -alltargets \
-  ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=NO SYMROOT="$SYMROOT" \
-  CLANG_ENABLE_MODULE_DEBUGGING=NO
+if [ $config != "ios" ];then
+  xcodebuild -project "$PODS_PROJECT" \
+    -sdk iphonesimulator -configuration Release -alltargets \
+    ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=NO SYMROOT="$SYMROOT" \
+    CLANG_ENABLE_MODULE_DEBUGGING=NO
+fi
